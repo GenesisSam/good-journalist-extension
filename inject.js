53e36229185c092722a2ce7dbc440238870b58c3
GenesisSam <samuel940401@gmail.com>
2019-01-14 16:34:50 +0900

    basic inject

---
+++ inject.js
@@ -0,0 +1,9 @@
+(function () {
+  var div = document.createElement('div');
+  div.style.position = 'fixed';
+  div.id = "genesissam"
+  div.style.top = 0;
+  div.style.right = 0;
+  div.textContent = 'hello, world';
+  document.body.appendChild(div);
+})();
